e511f9f61c6cc7eccd76f5d9ba23bb0e9b763110
Philiposobjj <165970578+Philiposobjj@users.noreply.github.com>
2024-10-23 20:25:54 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+import './App.css';
+import Cabecalho from './comum/componentes/Cabecalho/Cabecalho';
+import Rodape from './comum/componentes/Rodape/Rodape';
+
+import BotaoContador from './paginas/BotaoContador/BotaoContador';
+import ListaProdutos from './paginas/ListaProdutos/ListaProdutos';
+import PaginaInicial from './paginas/PaginaInicial/PaginaInicial';
+import PaginaListaTarefas from './paginas/PaginaListaTarefas/PaginaListaTarefas';
+
+const router = createBrowserRouter([
+  {
+    path: '',
+    element: <PaginaInicial />,
+  },
+  {
+    path: 'lista-produtos',
+    element: <ListaProdutos />,
+  },
+  {
+    path: 'botao-contador',
+    element: <BotaoContador />,
+  },
+  {
+    path: 'lista-tarefas',
+    element: <PaginaListaTarefas />,
+  },
+]);
+
+function App() {
+  return (
+    <>
+      <Cabecalho />
+      <RouterProvider router={router} />
+      <Rodape />
+    </>
+  );
+}
+
+export default App;
